b7c2e294e20fceedae3ca35d8b9e9b85bdbf74fb
Bryan Levin <bryanlevin72@gmail.com>
2023-07-31 12:35:53 -0700

    Intro (1)

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace Testing.Models
+{
+	public class ProductRepository:IProductRepository
+	{
+		private readonly IDbConnection _conn; //field
+
+        public ProductRepository(IDbConnection conn) //constructor
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;"); //query our BestBuy database
+        }
+    }
+}
+
