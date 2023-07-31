467116eaeaf6cd4e0af66516288daa6b66703b32
Bryan Levin <bryanlevin72@gmail.com>
2023-07-31 14:50:56 -0700

    Update Product (3)

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -23,6 +23,12 @@ namespace Testing.Models
         {
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
         }
+
+        void IProductRepository.UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+            new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
     }
 }
 
